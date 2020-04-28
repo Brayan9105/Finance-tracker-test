b12703f35a7dc7a74d7bad07f7bd152bd7174d45
Brayan Lopez <brayan9105@hotmail.com>
2020-04-28 15:13:59 -0500

    Added friend and follow actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :user_stocks, only: [:create, :destroy]
-  devise_for :users
   root 'welcome#index'
+
+  resources :user_stocks, only: [:create, :destroy]
+
+  devise_for :users
+  get 'my_friends', to: 'users#my_friends'
   get 'my_portafolio', to: 'users#my_portafolio'
   get 'search_stock', to: 'stocks#search'
+  get 'search_friend', to: 'users#search'
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
 end
